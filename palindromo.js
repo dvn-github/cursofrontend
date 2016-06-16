5e547f0da4ecf468a63ffacd4621d728c057ed64
DVN <dvalle@interesse.com.mx>
2016-06-15 20:42:47 -0500

    Se agrega funcion de minusculas sin ocupar métodos

--- palindromo.js
+++ palindromo.js
@@ -1,9 +1,22 @@
+function minusculas(texto){
+  var texto_ok = "";
+  var abecedario = {A : "a", "Á":"a", B : "b", C : "c", D : "d", E : "e", "É":"e", F : "f", G : "g", H : "h", I : "i", "Í":"i", J : "j", K : "k", L : "l", M : "m", N : "n", Ñ : "ñ", O : "o", "Ó":"o", P : "p", Q : "q", R : "r", S : "s", T : "t", U : "u", "Ú":"u", V : "v", W : "w", X : "x", Y : "y", Z : "z"}; 
+  for(var i in texto){
+    if(texto[i] in abecedario){
+      texto_ok += abecedario[texto[i]];
+    } else {
+      texto_ok +=  texto[i];
+    }
+  }
+  return texto_ok;
+}
+
 function palindromo(palabra){
   var palabra_sin_espacios = "", palabra_volteada = "";
   var tam = 0;
   for(var i in palabra){
     if(palabra[i] != ' '){
-      palabra_sin_espacios += palabra[i].toLowerCase();
+      palabra_sin_espacios += minusculas(palabra[i]);
       tam++;
     }
   }
