17ea9425ada2b0292eba5427af92fc2b327899b3
DVN <dvalle@interesse.com.mx>
2016-06-15 20:59:54 -0500

    Se acompleta el de piedra papel con la funcion de mayusculas

--- piedraPapelTijera.js
+++ piedraPapelTijera.js
@@ -1,6 +1,19 @@
+function minusculas(texto){
+  var texto_ok = "";
+  var abecedario = {A : "a", "Á":"a", B : "b", C : "c", D : "d", E : "e", "É":"e", F : "f", G : "g", H : "h", I : "i", "Í":"i", J : "j", K : "k", L : "l", M : "m", N : "n", Ñ : "ñ", O : "o", "Ó":"o", P : "p", Q : "q", R : "r", S : "s", T : "t", U : "u", "Ú":"u", V : "v", W : "w", X : "x", Y : "y", Z : "z"}; 
+  for(var i in texto){
+    if(texto[i] in abecedario){
+      texto_ok += abecedario[texto[i]];
+    } else {
+      texto_ok +=  texto[i];
+    }
+  }
+  return texto_ok;
+}
+
 do{
   var jugador1 = prompt("piedra, papel o tijera");
-  jugador1 = jugador1.toLowerCase();
+  jugador1 = minusculas(jugador1);
   var aleatorio = Math.floor((Math.random()*3)+1);
   var jugador2 = aleatorio == 1 ? "papel" : (aleatorio == 2 ? "piedra" : "tijera");
   
@@ -31,7 +44,10 @@ do{
           alert("PERDISTE \n (" + jugador2 + ")");
         }
       break;
-    
+      
+      default:
+        alert("Esa madre no existe!!");
+      break;
     }
   }
       
